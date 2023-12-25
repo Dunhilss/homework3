5ae2d62a89fc4e0c4e0d2b5427c76160d8d3a1db
Dunhilss <treborg28@gmail.com>
2023-12-25 21:53:23 +0200

    First step

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-num = int(input("Enter the day of the week (1-7): "))
+number = int(input("Enter the day of the week (1-7): "))
+if number == 1:
+    print("\nMonday")
+elif number == 2:
+    print("\nTuesday")
+elif number ==3:
+    print("\nWednesday")
+elif number == 4:
+    print("\nThursday")
+elif number == 5:
+    print("\nFriday")
+elif number == 6:
+    print("\nSunday")
+elif number == 7:
+    print("\nSaturday")
+else:
+    print("Incorrect day of the week entered, try again.")
